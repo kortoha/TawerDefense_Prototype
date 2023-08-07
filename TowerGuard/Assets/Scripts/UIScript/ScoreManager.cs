b87d9b1f114bc28102dd439cf1ce40ce00f0a78f
Андрій <gign2.0@gmail.com>
2023-08-07 16:02:03 +0300

    Score system

---
+++ TowerGuard/Assets/Scripts/UIScript/ScoreManager.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager Instance { get; private set; }
+
+    public int score { get; private set; } = 0;
+    public int bestScore { get; private set; } = 0;
+
+    [SerializeField] private Text _scoreText;
+    [SerializeField] private Text _bestScoreText;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        bestScore = PlayerPrefs.GetInt("BestScore", 0);
+        UpdateScoreText();
+    }
+
+    public void IncreaseScore()
+    {
+        score++;
+        if (score > bestScore)
+        {
+            bestScore = score;
+            PlayerPrefs.SetInt("BestScore", bestScore);
+        }
+        UpdateScoreText();
+    }
+
+    private void UpdateScoreText()
+    {
+        _scoreText.text = "Score:\n" + score.ToString();
+        _bestScoreText.text = "Best Score:\n" + bestScore.ToString();
+    }
+
+    public void ResetScore()
+    {
+        score = 0;
+        UpdateScoreText();
+    }
+}
